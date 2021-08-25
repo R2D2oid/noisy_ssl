ddc7f839441668afae8ec4889b22bacf31b7641e
Zahra Vaseqi <zahra.vaseqi@mail.mcgill.ca>
2021-08-25 12:58:13 -0700

    sbatch script to train ssl, classifier, no ssl

--- commands.sh
+++ commands.sh
@@ -3,8 +3,8 @@
 ############# Pretrain SSL ###############
 ##########################################
 
-sbatch run_experiments_pretrain_ssl.sh moco
-sbatch run_experiments_pretrain_ssl.sh barlowtwins 
+sbatch sbatch_pretrain_ssl.sh moco
+sbatch sbatch_pretrain_ssl.sh barlowtwins 
 
 ##########################################
 ########### Train Classifier #############
@@ -36,27 +36,55 @@ sbatch sbatch_train_clf.sh asym 0.8 moco
 sbatch sbatch_train_clf.sh asym 0.9 moco
 
 # train classifier using pretrained barlowtwins 
-sbatch sbatch_train_clf.sh non 0.0 barlowtwins 
+sbatch sbatch_train_clf.sh non 0.0 barlowtwins   
 
-sbatch sbatch_train_clf.sh sym 0.1 barlowtwins  
-sbatch sbatch_train_clf.sh sym 0.2 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.3 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.4 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.5 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.6 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.7 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.8 barlowtwins 
-sbatch sbatch_train_clf.sh sym 0.9 barlowtwins 
+sbatch sbatch_train_clf.sh sym 0.1 barlowtwins   
+sbatch sbatch_train_clf.sh sym 0.2 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.3 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.4 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.5 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.6 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.7 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.8 barlowtwins  
+sbatch sbatch_train_clf.sh sym 0.9 barlowtwins  
 
-sbatch sbatch_train_clf.sh asym 0.1 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.2 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.3 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.4 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.5 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.6 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.7 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.8 barlowtwins 
-sbatch sbatch_train_clf.sh asym 0.9 barlowtwins 
+sbatch sbatch_train_clf.sh asym 0.1 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.2 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.3 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.4 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.5 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.6 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.7 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.8 barlowtwins  
+sbatch sbatch_train_clf.sh asym 0.9 barlowtwins  
 
 
+##########################################
+########### Train Classifier #############
+###########      NO SSL      #############
+##########################################
+
+# train classifier without SSL
+# sbatch sbatch_train_clf.sh noise_type noise_rate model_type num_epochs
+sbatch sbatch_train_clf.sh non 0.0 only_supervised 2000
+
+sbatch sbatch_train_clf.sh sym 0.1 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.2 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.3 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.4 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.5 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.6 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.7 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.8 only_supervised 2000
+sbatch sbatch_train_clf.sh sym 0.9 only_supervised 2000
+
+sbatch sbatch_train_clf.sh asym 0.1 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.2 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.3 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.4 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.5 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.6 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.7 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.8 only_supervised 2000
+sbatch sbatch_train_clf.sh asym 0.9 only_supervised 2000
 
