82892fee908dedf104bd8a3d25519a897de5c07d
Zahra Vaseqi <zahra.vaseqi@mail.mcgill.ca>
2021-10-04 17:10:13 -0400

    fixed random target flipping issue

--- dataloader.py
+++ dataloader.py
@@ -102,6 +102,9 @@ class NoisyCIFAR10_Subset(datasets.CIFAR10):
         ## add noise to clf split
         self.noise_rate = noise_rate
         self.noise_type = noise_type
+        
+        random.seed(42)
+        np.random.seed(42)
 
         if noise_rate <= 0:
             return
@@ -115,7 +118,6 @@ class NoisyCIFAR10_Subset(datasets.CIFAR10):
         # for each class add noise to noise_rate percentage of its samples 
         for c in range(n_classes):
             indeces = np.where(np.array(self.targets) == c)[0]
-            np.random.seed(42)
             noisy_samples_idx = np.random.choice(indeces, n_noisy_per_class, replace=False)            
             
             if noise_type == 'sym':
@@ -124,13 +126,11 @@ class NoisyCIFAR10_Subset(datasets.CIFAR10):
 
                 for idx in noisy_samples_idx:
                     # pick a new class from the remaining 9 classes at random as noisy class for this sample
-                    random.seed(42)
                     self.targets[idx] = random.choice(class_ids)
             elif noise_type == 'asym':
                 for idx in noisy_samples_idx:
                     # use current_class+1 as the noisy class with prob noise_rate
                     current_class = self.targets[idx]
-                    np.random.seed(42)
                     self.targets[idx] = np.random.choice([current_class, (current_class+1)%n_classes], p=[1-noise_rate, noise_rate])
             else:
                 raise ValueError(f'Undefined noise_type: {noise_type}!') 
